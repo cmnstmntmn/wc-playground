524529e4a5581f7ace0566feecfee56313580f3d
cmnstmntmn <constantin.angheloiu@gmail.com>
2020-12-24 00:06:04 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import meiosis from "../../meiosis-setup/mergerino";
 import { createApp } from "./app/index.js"
 import { Loading } from "./components/loading/index.js"
 
@@ -13,7 +12,7 @@ createApp()
     .then(app => {
         const componentElemName = "wc-playground";
 
-        const {states, actions} = meiosis({
+        const {states, actions} = Meiosis.mergerino.setup({
             stream: update,
             merge,
             app,
